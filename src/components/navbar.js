fafb968885ea0842641bf04ef3f47a8d3b25a233
dylanjian0 <dylanjian0@gmail.com>
2024-06-17 06:46:59 -0400

    navbar styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,10 +7,12 @@ export default function Navbar(){
         {title:"About",link:"/about"},
     ]
     return(
-        <div className="">
-            {links.map((link) =>(
-                <Link key={link.title} href={link.link}>{link.title}</Link>
-            ))}
+        <div className="flex items-center bg-blue-500 h-14">
+            <div className="text-xl flex justify-center w-full space-x-6">
+                {links.map((link) =>(
+                    <Link key={link.title} href={link.link}>{link.title}</Link>
+                ))}
+            </div>
         </div>
     );
 }
