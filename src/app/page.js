1fdc4c9b7c60aaf9389f960a3f163174d991e4dd
dylanjian0 <dylanjian0@gmail.com>
2024-06-18 09:13:26 -0400

    algs display format test

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import Navbar from '../components/navbar';
 export default function Home() {
   return (
     <>
-        <Navbar/>
-        <div className="flex text-center font-mono text-black min-h-screen bg-blue-100 justify-center items-center">
-          welcome to the 3x3x3 rubik's cube algorithm trainer<br></br>
-        </div>
+      <Navbar/>
+      <div className="flex text-center font-mono text-black min-h-screen bg-blue-100 justify-center items-center">
+        welcome to the 3x3x3 rubik's cube algorithm trainer<br></br>
+      </div>
     </>
   );
 }
--- src/pages/algorithms.js
+++ src/pages/algorithms.js
@@ -1,11 +1,29 @@
 import Navbar from '../components/navbar'
 
 export default function Algorithms(){
+    const algs=[
+        {alg:"yo1",steps:"yo2"},
+        {alg:"hi1",steps:"hi2"},
+        {alg:"bye1",steps:"bye2"},
+    ]
     return(
         <>
             <Navbar/>
             <div className="min-h-screen font-mono bg-blue-100 text-black flex items-center justify-center">
-                table goes here
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>alg</th>
+                            <th>steps</th>
+                        </tr>
+                        {algs.map((item)=>(
+                            <tr key={item.alg}>
+                                <td>{item.alg}</td>
+                                <td>{item.steps}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </>
     );
